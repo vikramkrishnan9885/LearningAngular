b8b00429bf58c5a11d6a5dd15a49f425621cfe62
Vikram Krishnan <kvikram9885@gmail.com>
2019-05-31 01:14:52 +0530

    Displaing Lists and editing master css

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
+//////////
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +10,11 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
+  //hero: Hero = {
+  //  id: 1,
+  //  name: 'Windstorm'
+  //};
+  heroes = HEROES;
 
   constructor() { }
 
