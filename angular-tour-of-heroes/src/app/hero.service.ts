120a74dd87ed2e7e3924df1b467cd6c2ff82f2b7
Vikram Krishnan <kvikram9885@gmail.com>
2019-05-31 16:33:37 +0530

    Getting started with Observables

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@angular/core';
 ////
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+// The HeroService must wait for the server to respond, 
+// getHeroes() cannot return immediately with hero data, 
+// and the browser will not block while the service waits.
+// HeroService.getHeroes() must have an asynchronous signature of some kind.
+// It can take a callback. It could return a Promise. 
+// It could return an Observable.
+// Observable is one of the key classes in the RxJS library.
+// Angular's HttpClient methods return RxJS Observables
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +22,13 @@ import { HEROES } from './mock-heroes';
 
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  //getHeroes(): Hero[] {
+  //  return HEROES;
+  //}
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+    // of(HEROES) returns an Observable<Hero[]> that emits a single value, 
+    // the array of mock heroes.
   }
 
   constructor() { }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -29,17 +29,45 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-  
+  // While you could call getHeroes() in the constructor, 
+  // that's not the best practice.
+  // Reserve the constructor for simple initialization 
+  // such as wiring constructor parameters to properties. 
+  // The constructor shouldn't do anything. 
+  // It certainly shouldn't call a function that makes HTTP requests 
+  // to a remote server as a real data service would.
+  // Instead, call getHeroes() inside the ngOnInit lifecycle hook and 
+  // let Angular call ngOnInit at an appropriate time after constructing a 
+  // HeroesComponent instance.
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
+  //getHeroes(): void {
+  //  this.heroes = this.heroService.getHeroes();
+  //}
   // This will not work in a real app. 
   // You're getting away with it now because the service currently returns mock heroes. 
   // But soon the app will fetch heroes from a remote server, 
   // which is an inherently asynchronous operation.
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+  // Observable.subscribe() is the critical difference.
+  // The previous version assigns an array of heroes to the component's 
+  // heroes property. The assignment occurs synchronously, as if the server 
+  // could return heroes instantly or the browser could freeze the UI while 
+  // it waited for the server's response.
+  // That won't work when the HeroService is actually making requests of a 
+  // remote server.
+  // The new version waits for the Observable to emit the array of heroes— 
+  // which could happen now or several minutes from now. 
+  // Then subscribe passes the emitted array to the callback, 
+  // which sets the component's heroes property.
+  // This asynchronous approach will work when the HeroService requests 
+  // heroes from the server.
+
 }
