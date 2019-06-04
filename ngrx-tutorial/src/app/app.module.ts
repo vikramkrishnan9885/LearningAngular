06bac3f0e2ec1a94ef72dae27903864dfc349a38
Vikram Krishnan <kvikram9885@gmail.com>
2019-06-04 23:37:47 +0530

    Changed level of environment variable

--- ngrx-tutorial/src/app/app.module.ts
+++ ngrx-tutorial/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './store/reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../../environments/environment';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
