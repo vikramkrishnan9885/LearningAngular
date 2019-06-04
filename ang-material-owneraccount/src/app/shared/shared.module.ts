853f531fa92e688a839a54d0e350589b20649bc9
Vikram Krishnan <kvikram9885@gmail.com>
2019-06-04 13:16:53 +0530

    Added Shared module functionality. In a real life scenario, this is how you should be doing it

--- ang-material-owneraccount/src/app/shared/shared.module.ts
+++ ang-material-owneraccount/src/app/shared/shared.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+///
+import { MaterialModule } from '../material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
+  exports:[
+    MaterialModule,
+    FlexLayoutModule
   ]
 })
 export class SharedModule { }
+
+/**
+ * Important: Because we now have the FlexLayoutModule and MaterialModule 
+ * inside of the shared module file, we don’t need them anymore in the app 
+ * and owner module files. Therefore, we can remove the FlexLayoutModule and 
+ * MaterialModule imports from the app and owner module files and just 
+ * import the SharedModule in both mentioned module files (app and owner).
+ */
